b7f5d852db9fd5337fefb199fd7f742fd6d54594
Ayaan Jilani <59445725+CS-ION@users.noreply.github.com>
2020-12-03 17:35:35 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,10 @@
-puzzle = '''copy
-paste 
-puzzle 
-here 
-as 
-multiline
-string'''
+puzzle = '''1
+2
+3
+4
+5'''
+
+#copy paste your puzzle input as multiline string
 
 L = puzzle.split('\n')
 L1 = []
@@ -16,11 +16,11 @@ X = len(L1)
 for I in range(X):
     for J in range(I,X):
         if L1[I]+L1[J] == 2020:
-            print(L1[I]*L1[J])
+            print('Part1',L1[I]*L1[J])
 
 #part2
 for I in range(X):
     for J in range(I,X):
         for M in range(J,X):
             if L1[I]+L1[J]+L1[M] == 2020:
-                print(L1[I]*L1[J]*L1[M])
+                print('Part2',L1[I]*L1[J]*L1[M])
